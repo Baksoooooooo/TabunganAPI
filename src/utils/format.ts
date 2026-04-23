47d7c35508dfa82c1eae5589822ba56f341a7d14
Rafy Aditya Putra <rafiaditya30976@gmail.com>
2026-04-23 19:41:15 +0700

    Selesai

--- src/utils/format.ts
+++
@@ -1,7 +0,0 @@
-export const formatRupiah = (uang: number): string => {
-  const format = new Intl.NumberFormat("id-ID", {
-    style: "currency",
-    currency: "IDR",
-  }).format(uang);
-  return format;
-};
